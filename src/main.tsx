3202b0f1b74f5531e0db2aed3b7bbdbe46d81e38
Rehan Aslam <rehan@hypt.app>
2025-03-01 23:36:28 +0500

    connect graphql server using aplollo client

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_GRAPHQL_URI,
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ApolloProvider client={client}>
+      <App />
     <App />
+    </ApolloProvider>
   </StrictMode>,
 )
